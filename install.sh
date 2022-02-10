9fd9f5f17b8ce154ab3fdc00e8c22c2265d9743d
yezzus <yezzus49@mgail.com>
2022-02-10 14:30:59 -0400

    features added

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privileges to run this script"
 
 myApps="herbstluftwm dzen2 libx11-dev libxft-dev libxinerama-dev zsh git neovim
-stow yarn fzf kitty rofi polybar flameshot"
+stow yarn kitty rofi polybar flameshot"
 aptitude update && aptitude install -y $myApps 
 
 stow herbstluftwm
@@ -16,6 +16,8 @@ stow kitty
 stow rofi
 stow polybar
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
